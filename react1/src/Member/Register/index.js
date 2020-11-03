d93327b45f0510ac8495cdb3b6d3969c7572a41a
jos4211 <q1w2e3123@naver.com>
2020-11-04 02:18:26 +0900

    회원가입 일단 추가

--- react1/src/Member/Register/index.js
+++ react1/src/Member/Register/index.js
@@ -1,13 +1,21 @@
-import React, { useReducer } from 'react';
+import React, {useEffect, useReducer } from 'react';
 import { Link } from "react-router-dom"
 import './CSS/Register.scss';
+import * as authAPI from "./auth";
 
 function reducer(state, action) {
-    if (action.type === "ERROR") {
-        return { ...state, error: "입력칸을 모두 입력해 주세요." }
-    } else if (action.type === "NOERROR") {
-        return { ...state, error: "" }
+    
+    switch(action.type){
+        case "ERROR":
+            return { ...state, error: "입력칸을 모두 입력해 주세요.", errortext: false  };
+        case "EMAILERROR":
+            return { ...state, error: "이메일을 입력해 주세요.", errortext: false }
+        case "CODESEND":
+            return { ...state, error: "코드가 전송되었습니다.", errortext: true }
+        case "NOERROR":
+            return { ...state, error: "" }
     }
+
     return {
         ...state,
         [action.name]: action.value
@@ -17,16 +25,19 @@ function reducer(state, action) {
 
 function Register() {
 
+    
+
     const [state, dispatch] = useReducer(reducer, {
         userid: '',
         username: '',
         password: '',
         email: '',
         passwordConfirm: '',
-        error: ''
+        error: '',
+        errortext: false
     });
 
-    const { userid, username, password, email, passwordConfirm, error } = state;
+    const { userid, username, password, email, passwordConfirm, error, errortext } = state;
 
 
     const onChange = e => {
@@ -37,16 +48,30 @@ function Register() {
     const onSubmit = (event) => {
 
 
-        console.log(error);
+        console.log(authAPI.register);
         event.preventDefault();
         if ([userid, username, password, email, passwordConfirm].includes("")) {
             dispatch({ type: "ERROR" });
             return;
         } else {
+            authAPI.register(userid, username, password, email);
             dispatch({ type: "NOERROR" });
             return;
         }
     };
+
+    const codeSend = (event) =>{
+        event.preventDefault();
+        
+        if(email === ""){
+            dispatch({ type: "EMAILERROR" });
+            return;
+        }else {
+            dispatch({ type: "CODESEND" });
+            return;
+        }
+
+    }
 
 
 
@@ -60,7 +85,7 @@ function Register() {
                     <p><input className="regist_inputBox regist_id" name="userid" type="text" placeholder="아이디" value={userid} onChange={onChange} autoComplete="off"></input></p>
                     <p>
                         <input className="regist_inputBox regist_email" name="email" type="email" placeholder="이메일" value={email} onChange={onChange} autoComplete="off"></input>
-                        <button className="codeBtn regist_email_codeSend mt-1" >코드전송</button>
+                        <button className="codeBtn regist_email_codeSend mt-1" onClick={codeSend}>코드전송</button>
                     </p>
                     <p>
                         <input className="regist_email_code ml-2" type="text" placeholder="인증 코드" autoComplete="off"></input>
@@ -68,7 +93,8 @@ function Register() {
                     </p>
                     <p><input className="regist_inputBox regist_pass" name="password" type="password" placeholder="비밀번호" value={password} onChange={onChange} autoComplete="off"></input></p>
                     <p><input className="regist_inputBox regist_pass" name="passwordConfirm" type="password" placeholder="비밀번호 확인" value={passwordConfirm} onChange={onChange} autoComplete="off"></input></p>
-                    <p className="errortext">{error}</p>
+                    
+                    {errortext?<p className="text-white">{error}</p> : <p className="text-warning">{error}</p> }
                     <p>
                         <button className="regist_btn">회원가입</button>
                         <Link to="/"><button className="regist_back">돌아가기</button></Link>
