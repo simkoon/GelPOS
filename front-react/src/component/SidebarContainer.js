241aa1ed99f40a70ba067c4449a3c08d4413c06e
unknown <q1w2e3123@naver.com>
2020-11-16 13:36:43 +0900

    20201116 테이블 틀 만듦 - 조옥상

--- front-react/src/component/SidebarContainer.js
+++ front-react/src/component/SidebarContainer.js
@@ -1,8 +1,9 @@
-import { Route } from 'react-router-dom';
-import Sidebar from '../comm/Sidebar/Tsidebar';
-import Scheduler from './Scheduler';
-import StoreAdd from './StoreAdd/StoreAddContainer';
-import Invoice from './Invoice/Invoice';
+import { Route } from "react-router-dom";
+import Sidebar from "../comm/Sidebar/Tsidebar";
+import Scheduler from "./Scheduler";
+import StoreAdd from "./StoreAdd/StoreAddContainer";
+import Invoice from "./Invoice/Invoice";
+import Table from "./Table";
 
 export default function SidebarContainer(params) {
   return (
@@ -11,6 +12,7 @@ export default function SidebarContainer(params) {
         <Route path="/store/scheduler" component={Scheduler} />
         <Route path="/store/storeAdd" component={StoreAdd} />
         <Route path="/store/invoice" component={Invoice} />
+        <Route path="/store/table" component={Table} />
       </Sidebar>
     </>
   );
