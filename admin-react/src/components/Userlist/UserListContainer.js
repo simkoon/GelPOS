76e7c3b1695546cb40ed2d6a323c163eae38d5c5
Spice7 <airforce940706@gmail.com>
2020-11-20 02:28:26 +0900

    관리자하는중

--- admin-react/src/components/Userlist/UserListContainer.js
+++ admin-react/src/components/Userlist/UserListContainer.js
@@ -5,3 +5,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import UserListTable from './UserListTable';
 import {listUserlists} from '../../modules/userlists';
 
+const UserListContainer = ({ location }) => {
+    const dispatch = useDispatch();
+    const { userlists, error, loading } = useSelector(
+      ({ userlists, loading }) => ({
+        userlists: userlists.userlists,
+        error: userlists.error,
+        loading: loading['userlists/LIST_USERLISTS'],
+      }),
+    );
+    useEffect(() => {
+      const { page, userid, username, email } = qs.parse(location.search, {
+        ignoreQueryPrefix: true,
+      });
+      dispatch(listUserlists({ page, userid, username, email }));
+    }, [dispatch, location.search]);
+  
+    return (
+      <UserListTable
+        loading={loading}
+        error={error}
+        userlists={userlists}
+      />
+    );
+  };
+  
+  export default withRouter(UserListContainer);
