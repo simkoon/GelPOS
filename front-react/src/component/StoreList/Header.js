de07ae97261cb20d9c466a714cc2c05b3dac51a5
simkoon <shk321233@naver.com>
2020-11-26 17:50:51 +0900

    20201126 헤더 홈으로 고객센터 삭제 -심호길

--- front-react/src/component/StoreList/Header.js
+++ front-react/src/component/StoreList/Header.js
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
-import { Navbar, Nav } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function Header({ onLogout }) {
   return (
@@ -9,16 +9,16 @@ function Header({ onLogout }) {
         expand="lg"
         style={{
           flex: 1,
-          backgroundColor: "rgb(251,252,254)",
-          boxSizing: "border-box",
+          backgroundColor: 'rgb(251,252,254)',
+          boxSizing: 'border-box',
         }}
       >
-        {" "}
+        {' '}
         <Link to="/">
           <Navbar.Brand
             className="font-weight-bolder"
             style={{
-              color: "rgb(62,78,93)",
+              color: 'rgb(62,78,93)',
             }}
           >
             GELPOS
@@ -27,28 +27,10 @@ function Header({ onLogout }) {
         <Navbar.Toggle aria-controls="basic-navbar-nav " />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Link
-              to="/"
-              style={{
-                color: "rgb(62,78,93)",
-              }}
-              className="small font-weight-normal nav-link"
-            >
-              홈으로
-            </Link>
-            <Link
-              to="/"
-              className="small font-weight-normal nav-link"
-              style={{
-                color: "rgb(62,78,93)",
-              }}
-            >
-              고객센터
-            </Link>
             <div
               style={{
-                color: "rgb(62,78,93)",
-                cursor: "pointer",
+                color: 'rgb(62,78,93)',
+                cursor: 'pointer',
               }}
               className=" small font-weight-normal nav-link"
               onClick={onLogout}
